2da73500a7bae0fcba77a3906eb0a01f4ae2757a
Mihndim <jelsieprisca2016@gmail.com>
2021-02-26 17:27:29 -0500

    Initialize board class

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,20 @@
-def player 
-    #attributes 
-    #define wining combination 
-    #win_status
-def initialize
-    #players name 
-    #players marks
-    #display collection should be an array 
-def make_move 
-    #value and board as parameters
-    #display board 
-def check_win 
-    #variable = false
-    #check if the collection is = 3
-    #check possible wining combination 
-end
+class Player
+    WINNING_COMBINATIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
+    attr_accessor :name
+    def player 
+        #attributes 
+        #define wining combination 
+        #win_status
+    def initialize
+        #players name 
+        #players marks
+        #display collection should be an array 
+    def make_move 
+        #value and board as parameters
+        #display board 
+    def check_win 
+        #variable = false
+        #check if the collection is = 3
+        #check possible wining combination 
+    end
+end    
