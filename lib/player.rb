613f19bafe78499c70d33fca92fda7ce25e5b491
Mihndim <jelsieprisca2016@gmail.com>
2021-03-01 10:46:23 -0500

    Initialize method for player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,14 @@
 class Player
     WINNING_COMBINATIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
     attr_accessor :name
-    def player 
-        #attributes 
-        #define wining combination 
-        #win_status
+    attr_writer :selection
+    
+    def initialize(name, marker)
+        @name = name
+        @marker = marker
+        @selection = []
+    end
+
     def initialize
         #players name 
         #players marks
