3991761ec46cdba112c0dde7068e09b737145f2d
angelacuahutle <angela.cuahutle@gmail.com>
2021-03-25 14:21:55 -0600

    basic test cases for board class

--- spec/game_test.rb
+++ spec/game_test.rb
@@ -2,3 +2,23 @@
 
 require_relative '../lib/player'
 require_relative '../lib/board'
+
+describe Board do
+  describe "#check_draw" do
+    it "checks a draw" do
+      # example conditions: let(:variable) { ... }
+      # test_board = Board.new
+      # player 1 moves
+      # player 2 moves
+      expect(available_slots.empty?).to be true
+    end
+
+    it "does not check a draw" do
+      # example conditions: let(:variable) { ... }
+      # test_board = Board.new
+      # player 1 moves
+      # player 2 moves
+      expect(available_slots.empty?).to be false
+    end
+  end
+end
